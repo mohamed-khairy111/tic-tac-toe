e16ec4fdf03099ce9244b9ae8f1b13e665417f32
mohamed khairy <mohamed khairy>
2024-11-09 19:27:38 +0200

    ...

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
@@ -6,7 +7,7 @@ const char PLAYER_1 = 'O';
 const char PLAYER_2 = 'X';
 char board[3][3];
 
-void Insitialise(char board[][3])
+void Insitialise()
 {
     for (int i = 0; i < 3; i++)
     {
@@ -17,7 +18,7 @@ void Insitialise(char board[][3])
     }
 }
 
-void Display(char board[][3])
+void Display()
 {
     for (int i = 0; i < 3; i++)
     {
@@ -29,9 +30,47 @@ void Display(char board[][3])
     }
 }
 
+void SetValue(int row, int col, char symbol)
+{
+    board[row][col] = symbol;
+}
+
+bool IsFree(int row, int col)
+{
+    if (board[row][col] == '-')
+        return false;
+    else
+        return true;
+}
+
 int main()
 {
-    Insitialise(board);
-    Display(board);
+    srand(time(NULL));
+    bool gameOver = false;
+    int row = 0;
+    int col = 0;
+    char player = PLAYER_1;
+    int count = 0;
+    Insitialise();
+
+    while (!gameOver)
+    {
+        row = rand() % 3;
+        col = rand() % 3;
+        while (!IsFree(row, col))
+        {
+            SetValue(row, col, player);
+            if (player == PLAYER_1)
+                player = PLAYER_2;
+            else
+                player = PLAYER_1;
+            count++;
+            if (count == 9)
+                gameOver = true;
+            Display();
+            cout << "________________" << endl;
+        }
+    }
+
     return 0;
 }
