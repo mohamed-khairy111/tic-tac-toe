bc2c7f82635472055b0c1c073ceda26f3fcf8040
mohamed khairy <mohamed khairy>
2024-11-10 14:14:34 +0200

    finished the game.

--- main.cpp
+++ main.cpp
@@ -5,9 +5,16 @@ using namespace std;
 
 const char PLAYER_1 = 'O';
 const char PLAYER_2 = 'X';
-char board[3][3];
+int winningStates[8][3][2] = {{{0, 0}, {0, 1}, {0, 2}},
+                              {{1, 0}, {1, 1}, {1, 2}},
+                              {{2, 0}, {2, 1}, {2, 2}},
+                              {{0, 0}, {1, 0}, {2, 0}},
+                              {{0, 1}, {1, 1}, {2, 1}},
+                              {{0, 2}, {1, 2}, {2, 2}},
+                              {{0, 0}, {1, 1}, {2, 2}},
+                              {{0, 2}, {1, 1}, {2, 0}}};
 
-void Insitialise()
+void Initialise(char board[3][3])
 {
     for (int i = 0; i < 3; i++)
     {
@@ -18,7 +25,7 @@ void Insitialise()
     }
 }
 
-void Display()
+void Display(char board[3][3]) // board is read-only in Display
 {
     for (int i = 0; i < 3; i++)
     {
@@ -30,46 +37,83 @@ void Display()
     }
 }
 
-void SetValue(int row, int col, char symbol)
+void SetValue(char board[3][3], int row, int col, char symbol)
 {
     board[row][col] = symbol;
 }
 
-bool IsFree(int row, int col)
+bool IsFree(char board[3][3], int row, int col) // board is read-only in IsFree
 {
-    if (board[row][col] == '-')
-        return false;
-    else
-        return true;
+    return board[row][col] == '-';
+}
+
+bool HasWon(char board[3][3], char symbol) // board is read-only in HasWon
+{
+    for (int m = 0; m <= 7; m++)
+    {
+        bool match = true;
+        for (int s = 0; s <= 2; s++)
+        {
+            int row = winningStates[m][s][0];
+            int col = winningStates[m][s][1];
+            if (board[row][col] != symbol)
+            {
+                match = false;
+                break;
+            }
+        }
+        if (match)
+            return true;
+    }
+    return false;
 }
 
 int main()
 {
     srand(time(NULL));
+    char board[3][3];
     bool gameOver = false;
     int row = 0;
     int col = 0;
     char player = PLAYER_1;
     int count = 0;
-    Insitialise();
+    Initialise(board);
 
     while (!gameOver)
     {
-        row = rand() % 3;
-        col = rand() % 3;
-        while (!IsFree(row, col))
+        Display(board);
+        cout << "Player " << player << ", enter the row number (0-2): ";
+        cin >> row;
+        cout << "Enter the col number (0-2): ";
+        cin >> col;
+
+        if (row < 0 || row > 2 || col < 0 || col > 2 || !IsFree(board, row, col))
         {
-            SetValue(row, col, player);
-            if (player == PLAYER_1)
-                player = PLAYER_2;
-            else
-                player = PLAYER_1;
-            count++;
-            if (count == 9)
-                gameOver = true;
-            Display();
-            cout << "________________" << endl;
+            cout << "Invalid move. Try again." << endl;
+            continue;
         }
+
+        SetValue(board, row, col, player);
+        count++;
+
+        if (HasWon(board, player))
+        {
+            Display(board);
+            cout << "Player " << player << " HAS WON!" << endl;
+            gameOver = true;
+        }
+        else if (count == 9)
+        {
+            Display(board);
+            cout << "\tDRAW!" << endl;
+            gameOver = true;
+        }
+        else
+        {
+            player = (player == PLAYER_1) ? PLAYER_2 : PLAYER_1;
+        }
+
+        cout << "________________" << endl;
     }
 
     return 0;
